79a22d3bb918eb4d854997b2d55564ae9509182c
SherazHashmi1111 <sherazhashmi111@gmail.com>
2025-10-27 15:12:09 +0500

    Section banner is completed

--- app/(root)/(website)/page.jsx
+++ app/(root)/(website)/page.jsx
@@ -1,6 +1,8 @@
 import MainSlider from "@/components/application/website/MainSlider";
 import Banner from "@/components/application/website/Banner";
 import FeaturedProduct from "@/components/application/website/FeaturedProduct";
+import Image from "next/image";
+import advertisingBanner from "@/public/assets/images/advertising-banner.png";
 
 const Home = () => {
   return (
@@ -9,10 +11,19 @@ const Home = () => {
         <MainSlider />
       </section>
       <section className="lg:px-32 px-4 sm:pt20 pt-5 pb-10">
-        <Banner/>
+        <Banner />
       </section>
       <section className="lg:px-32 px-4 sm:pt20 pt-5 pb-10">
-        <FeaturedProduct/>
+        <FeaturedProduct />
+      </section>
+      <section className="sm:pt20 pt-5 pb-10">
+        <Image
+          src={advertisingBanner.src}
+          height={advertisingBanner.height}
+          width={advertisingBanner.width}
+          alt="advertisingBanner"
+          className="h-full w-full object-cover"
+        />
       </section>
     </div>
   );
